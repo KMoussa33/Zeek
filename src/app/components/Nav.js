ed78b1781c9542d3909ab33c1306516161a4cba3
Zach Gallman <zach.ecab@gmail.com>
2023-06-28 16:53:06 -0400

    Added Components and dashboard folder. Created boilderplate code for the nav and loginModal components.

---
+++ src/app/components/Nav.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import Link from "next/Link";
+import "../../styles/globals.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+
+export default function Nav() {
+  // toggle faBars for small to medium screens
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <nav>
+      <div>
+        <Link href="/">
+          <h1>ZeeK</h1>
+        </Link>
+        {/* hides fabars larger than medium screens */}
+        <button className="md:hidden" onClick={toggleMenu}>
+          <FontAwesomeIcon icone={faBars} className="text-stone-700" />
+        </button>
+        <div className={`md:flex items center ${isOpen ? "" : "hidden"}`}>
+          {/* Dynamic Search bar */}
+          <div className="search-wrapper">
+            <div className="input-holder">
+              <input
+                type="text"
+                className="search-input"
+                placeholder="Type to search"
+              />
+              <button
+                className="search-icon"
+                onClick="searchToggle(this, event);"
+              >
+                <span></span>
+              </button>
+            </div>
+            <span className="close" onClick="searchToggle(this, event"></span>
+          </div>
+          <div>
+            <button>
+              <FontAwesomeIcon icon="fa-solid fa-right-to-bracket" />
+            </button>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
