847415d95389040cd83920957de8422514d4a8d1
Zach Gallman <zach.ecab@gmail.com>
2023-06-29 08:31:32 -0400

    Made boiler code for home page

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,10 +5,10 @@ import Footer from "./components/Footer";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <div className="flex flex-col min-h-screen max-w-2xl px-3 pt-5 pb-10">
+      <div className="flex flex-col mx-auto ">
         <div className="flex-grow">
           <Header />
-          <body className="my-0 py-10">{children}</body>
+          <main className="my-0 ">{children}</main>
         </div>
         <Footer />
       </div>
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,28 @@
 import React from "react";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className="">
+      <div className="container flex mx-auto">
+        <div className="mx-auto">
+          <h1>Featured</h1>
+          {/* Below will be where the featured games section populates */}
+          <div className="bg-stone-300">
+            <ul>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+            </ul>
+            <ul>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
