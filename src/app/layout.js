209c6fbf5249dbe9c9c313f83f5825affd309029
Zach Gallman <zach.ecab@gmail.com>
2023-06-28 20:01:57 -0400

    Create an nvmrc file to hold the current version of Node

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 export default function RootLayout({ children }) {
   return (
